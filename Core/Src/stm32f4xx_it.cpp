5b1fa77480e76fa33f4d8bb68252840990c0cd47
joaquinalvidrez95 <joaquinalvidrez95@gmail.com>
2020-11-01 23:27:53 -0500

    Enclose extern "C"

--- Core/Src/stm32f4xx_it.cpp
+++ Core/Src/stm32f4xx_it.cpp
@@ -13,21 +13,24 @@ namespace interrupt
 
 } // namespace interrupt
 
-extern "C" void SysTick_Handler(void)
+extern "C"
 {
-    HAL_IncTick();
-    HAL_SYSTICK_IRQHandler();
-}
-
-extern "C" void TIM6_DAC_IRQHandler(void)
-{
-    if (nullptr != interrupt::gph_timer)
+    void SysTick_Handler(void)
     {
-        HAL_TIM_IRQHandler(interrupt::gph_timer);
+        HAL_IncTick();
+        HAL_SYSTICK_IRQHandler();
+    }
+
+    void TIM6_DAC_IRQHandler(void)
+    {
+        if (nullptr != interrupt::gph_timer)
+        {
+            HAL_TIM_IRQHandler(interrupt::gph_timer);
+        }
     }
 }
 
-extern "C" void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 }
